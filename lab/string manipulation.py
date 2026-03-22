120c927a63efe6150d874e4d1caa240b3ec149ae
manish <yadavdn72@gmail.com>
2026-03-22 10:14:04 +0530

    assignment

---
+++ lab/string manipulation.py
@@ -0,0 +1,8 @@
+# This program shows string operations
+
+s = "python"
+
+print(s.upper())      # Convert to uppercase
+print(s.lower())      # Convert to lowercase
+print(s[1:4])         # Slicing
+print(s + " lang")    # Concatenation
