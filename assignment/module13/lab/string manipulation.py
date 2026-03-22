ecf83e85ded9877e2a8f53c699eb47ede8c870ab
manishkumaryadav8733 <yadavdn72@gmail.com>
2026-03-22 09:52:53 +0530

    Delete assignment/module13 directory

--- assignment/module13/lab/string manipulation.py
+++
@@ -1,8 +0,0 @@
-# This program shows string operations
-
-s = "python"
-
-print(s.upper())      # Convert to uppercase
-print(s.lower())      # Convert to lowercase
-print(s[1:4])         # Slicing
-print(s + " lang")    # Concatenation
